bc2ab4bbb989357066a6bc728306be4b23981f1f
shota nakashima <fantajista5.6nakaji.s@gmail.com>
2021-10-06 13:50:50 +0900

    setup storyshots

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  roots: ["<rootDir>"],
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest",
+    '^.+\\.stories\\.tsx$': '@storybook/addon-storyshots/injectFileName',
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
   },
-  watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname",
-  ],
+  testMatch: ['<rootDir>/**/?(*.)(spec|test).(ts|js)?(x)'],
+  //jestのversionが未対応のためjest-watch-typeaheadをuninstallした(jest-watch-typeaheadはjestv27~)
+  // watchPlugins: [
+  //   'jest-watch-typeahead/filename',
+  //   'jest-watch-typeahead/testname',
+  // ],
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    '\\.(styl|css|less|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
-};
+  moduleDirectories: ['node_modules', 'src'],
+}
